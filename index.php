17858bf7e041d52625540c5c39e74ec2ad2f7ee4
Kuater <94171024+Kuater@users.noreply.github.com>
2022-05-28 19:31:24 +0100

    Add files via upload

---
+++ index.php
@@ -0,0 +1,28 @@
+<?php
+require '';
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Kaos</title>
+</head>
+
+<body>
+    <section class="landingPage">
+        <form action="" method="post" class="formConnexion">
+            <input type="email" name="email" id="email" placeholder="E-mail" required>
+            <input type="password" name="mdp" id="mdp" placeholder="Mot de passe" required>
+            <input type="submit" class="btnConnexion" value="Se connecter">
+        </form>
+        <a href="formulaire.php"><button class="btnInscription">S'inscrire</button></a>
+
+    </section>
+
+</body>
+
+</html>
